cfa8010ecaa3cc6d9c3b55bd8aa877c5a77e4213
Ruth Ambaw <106428054+ruth610@users.noreply.github.com>
2025-08-13 21:10:59 +0300

    Created 'Find Center of Star Graph' by A2SV Hub

---
+++ 13-Aug-2025/Find Center of Star Graph 384696.py
@@ -0,0 +1,12 @@
+# Problem: Find Center of Star Graph - https://leetcode.com/problems/find-center-of-star-graph/
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        first = edges[0][0]
+        second = edges[0][1]
+
+        if edges[1][0] == first or edges[1][1] == first:
+            return first
+        return second
+
+
